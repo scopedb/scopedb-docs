27332cad7d7aa2178f13ced0b971dacec06a17ab
tison <wander4096@gmail.com>
2025-03-27 08:53:27 +0800

    fix: try astro-rehype-relative-markdown-links
    Signed-off-by: tison <wander4096@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import mdx from '@astrojs/mdx';
 import { defineConfig } from 'astro/config';
+import rehypeAstroRelativeMarkdownLinks from "astro-rehype-relative-markdown-links";
 
 // https://astro.build/config
 export default defineConfig({
+    markdown: {
+        rehypePlugins: [rehypeAstroRelativeMarkdownLinks],
+    },
     integrations: [mdx()],
     redirects: {
         "/": "/reference/overview",
