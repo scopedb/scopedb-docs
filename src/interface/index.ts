f36512ba19b60285c82857ee916fa8868ecb822c
Artea <sepush@outlook.com>
2025-03-30 21:26:32 +0800

    feat: navbar

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -3,3 +3,10 @@ import type {MarkdownHeading} from "astro";
 export interface TreeTocNode extends MarkdownHeading {
     children: TreeTocNode[];
 }
+
+export interface NavItemProps{
+    text: string;
+    link: string;
+}
+
+export type NavItemsProps = NavItemProps[];
