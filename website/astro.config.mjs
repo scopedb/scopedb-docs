bf0eccaaaa17bbeac2c9b71f43ae57049ab86ddd
STRRL <im@strrl.dev>
2025-04-13 13:03:57 -0700

    chore: configure tailwindcss and component overwrites
    Signed-off-by: STRRL <im@strrl.dev>

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -4,19 +4,28 @@ import { defineConfig } from 'astro/config';
 import starlightLinksValidator from 'starlight-links-validator';
 import { sidebar } from './astro.sidebar.ts';
 import { devServerFileWatcher } from './config/integrations/dev-server-file-watcher.ts';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		devServerFileWatcher([
-			'./config/*',
-			'./astro.sidebar.ts',
-		]),
-		starlight({
-			title: 'ScopeDB',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/scopedb/scopedb-docs' }],
-			plugins: [starlightLinksValidator()],
-			sidebar
-		}),
+  integrations: [
+      devServerFileWatcher([
+          './config/*',
+          './astro.sidebar.ts',
+      ]),
+      starlight({
+          title: 'ScopeDB',
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/scopedb/scopedb-docs' }],
+          plugins: [starlightLinksValidator()],
+          customCss: ['./src/styles/global.css'],
+          sidebar,
+          components:{
+            'PageFrame': './src/overrides/PageFrame.astro'
+          }
+      }),
 	],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
