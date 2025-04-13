8ddc5a9261079106d37753f69485f3dda511d819
STRRL <im@strrl.dev>
2025-04-13 14:45:17 -0700

    chore: stylized header and footer on desktop
    Signed-off-by: STRRL <im@strrl.dev>

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -9,21 +9,22 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-      devServerFileWatcher([
-          './config/*',
-          './astro.sidebar.ts',
-      ]),
-      starlight({
-          title: 'ScopeDB',
-          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/scopedb/scopedb-docs' }],
-          plugins: [starlightLinksValidator()],
-          customCss: ['./src/styles/global.css'],
-          sidebar,
-          components:{
-            'PageFrame': './src/overrides/PageFrame.astro'
-          }
-      }),
-	],
+    devServerFileWatcher([
+      './config/*',
+      './astro.sidebar.ts',
+    ]),
+    starlight({
+      title: 'ScopeDB',
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/scopedb/scopedb-docs' }],
+      plugins: [starlightLinksValidator()],
+      customCss: ['./src/styles/global.css'],
+      sidebar,
+      components: {
+        'Header': './src/overrides/Header.astro',
+        'PageFrame': './src/overrides/PageFrame.astro',
+      }
+    }),
+  ],
 
   vite: {
     plugins: [tailwindcss()],
