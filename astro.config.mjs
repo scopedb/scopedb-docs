5da2ba4a7675a7ca808619d20954378e43f2b59d
Artea <sepush@outlook.com>
2025-04-28 19:00:50 +0800

    feat: home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import {defineConfig} from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 
