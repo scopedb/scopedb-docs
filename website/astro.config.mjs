bb9cbf011d7ff50b7eb498dad5961a8d796c22eb
STRRL <im@strrl.dev>
2025-04-29 20:45:11 -0700

    chore: update style
    Signed-off-by: STRRL <im@strrl.dev>

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,59 +1,64 @@
 // @ts-check
-import starlight from '@astrojs/starlight';
-import { defineConfig } from 'astro/config';
-import starlightLinksValidator from 'starlight-links-validator';
-import { sidebar } from './astro.sidebar.ts';
-import { devServerFileWatcher } from './config/integrations/dev-server-file-watcher.ts';
-import tailwindcss from '@tailwindcss/vite';
-import starlightSidebarTopics from 'starlight-sidebar-topics'
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
+import starlightLinksValidator from "starlight-links-validator";
+import { sidebar } from "./astro.sidebar.ts";
+import { devServerFileWatcher } from "./config/integrations/dev-server-file-watcher.ts";
+import tailwindcss from "@tailwindcss/vite";
+import starlightSidebarTopics from "starlight-sidebar-topics";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    devServerFileWatcher([
-      './config/*',
-      './astro.sidebar.ts',
-    ]),
+    devServerFileWatcher(["./config/*", "./astro.sidebar.ts"]),
     starlight({
-      title: 'ScopeDB',
-      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/scopedb/scopedb-docs' }],
+      title: "ScopeDB",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/scopedb/scopedb-docs",
+        },
+      ],
       plugins: [
         starlightSidebarTopics([
           {
-            label: 'Developer',
-            link: '/developer',
-            items: []
+            label: "Developer",
+            link: "/developer",
+            items: [],
           },
           {
-            label: 'Reference',
-            link: '/reference',
+            label: "Reference",
+            link: "/reference",
+            items: [...sidebar],
+          },
+          {
+            label: "Releases",
+            link: "/releases",
             items: [
-              ...sidebar
-            ]
+              {
+                label: "ScopeDB Releases",
+                link: "/releases",
+              },
+            ],
           },
           {
-            label: 'Releases',
-            link: '/releases',
-            items: [{
-              label: 'ScopeDB Releases',
-              link: '/releases'
-            }]
-          },
-          {
-            label: 'Tutorials',
-            link: '/tutorials',
-            items: []
+            label: "Tutorials",
+            link: "/tutorials",
+            items: [],
           },
         ]),
-        starlightLinksValidator()],
-      customCss: ['./src/styles/global.css'],
-      // sidebar,
+        starlightLinksValidator(),
+      ],
+      customCss: ["./src/styles/global.css"],
       components: {
-        'Header': './src/overrides/Header.astro',
-        'PageFrame': './src/overrides/PageFrame.astro',
-        'Sidebar': './src/overrides/Sidebar.astro',
-        'PageSidebar': './src/overrides/PageSidebar.astro'
-      }
+        Header: "./src/overrides/Header.astro",
+        PageFrame: "./src/overrides/PageFrame.astro",
+        Sidebar: "./src/overrides/Sidebar.astro",
+        PageSidebar: "./src/overrides/PageSidebar.astro",
+        TwoColumnContent: "./src/overrides/TwoColumnContent.astro",
+        ContentPanel: "./src/overrides/ContentPanel.astro",
+      },
     }),
   ],
 
