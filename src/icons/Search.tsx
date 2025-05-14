1aa7c6c94affacfddf78ba5a0ca7ea72607306e8
Artea <sepush@outlook.com>
2025-05-14 22:32:57 +0800

    chore: more icon

--- src/icons/Search.tsx
+++ src/icons/Search.tsx
@@ -1,18 +1,4 @@
+import Search from './search.svg?react'
 export function SearchIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      {...props}
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        d="M10.5 3a7.5 7.5 0 100 15 7.5 7.5 0 000-15zM21 21l-4.35-4.35"
-      />
-    </svg>
-  );
+  return <Search {...props}/>;
 }
