94fcb734cfc2c65e8e96bc8aabc7e7b230065233
tison <wander4096@gmail.com>
2025-06-16 20:12:13 +0800

    chore: code tidy
    Signed-off-by: tison <wander4096@gmail.com>

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,32 +1,30 @@
 import { z, defineCollection } from "astro:content";
 
+const schema = z.object({
+  title: z.optional(z.string()),
+  relatedContents: z.optional(
+    z.array(
+      z.object({
+        title: z.string(),
+        url: z.string(),
+      }),
+    ),
+  ),
+});
+
 const guidesCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.optional(z.string()),
-  }),
+  schema: schema,
 });
 
 const developerCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.optional(z.string()),
-  }),
+  schema: schema,
 });
 
 const referenceCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.optional(z.string()),
-    relatedContents: z.optional(
-      z.array(
-        z.object({
-          title: z.string(),
-          url: z.string(),
-        }),
-      ),
-    ),
-  }),
+  schema: schema,
 });
 
 export const collections = {
--- src/content/reference/functions/date-and-time/temporal-trunc.md
+++ src/content/reference/functions/date-and-time/temporal-trunc.md
@@ -1,5 +1,5 @@
 ---
-title: temporal-trunc
+title: trunc
 ---
 
 # trunc
