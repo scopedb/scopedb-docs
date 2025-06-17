db7f65694d9d2fdb8eb2f48688408d683516669a
tison <wander4096@gmail.com>
2025-06-17 20:56:26 +0800

    chore: no need local css module hack
    Signed-off-by: tison <wander4096@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import process from "node:process";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
@@ -13,11 +12,6 @@ import remarkCalloutDirectives from "@microflash/remark-callout-directives";
 import devtoolsJson from "vite-plugin-devtools-json";
 import scopeql from "./shiki-scopeql-grammar.json";
 
-function viteCssModuleName() {
-  // do not use hash when dev
-  return process.env.NODE_ENV === "development" ? "[local]" : "[hash:base64:8]";
-}
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://docs.scopedb.io",
@@ -27,11 +21,6 @@ export default defineConfig({
     plugins: [tailwindcss(), devtoolsJson()],
     optimizeDeps: {
       include: ["@docsearch/react"],
-    },
-    css: {
-      modules: {
-        generateScopedName: viteCssModuleName(),
-      },
     },
   },
 
