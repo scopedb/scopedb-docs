64a0ec92e6334b66ea05e23cfbe4d12f81ab0e24
Yu Lei <leiysky@outlook.com>
2026-07-11 17:11:52 +0800

    fix: use next links for docs mdx links (#22)

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,9 +1,28 @@
 import type { MDXComponents } from 'mdx/types'
+import Link from 'next/link'
+import type { AnchorHTMLAttributes } from 'react'
 import Callout from './components/Callout'
+
+function MarkdownLink({ href, children, ...props }: AnchorHTMLAttributes<HTMLAnchorElement>) {
+    if (typeof href === 'string' && href.startsWith('/') && !href.startsWith('//')) {
+        return (
+            <Link href={href} {...props}>
+                {children}
+            </Link>
+        )
+    }
+
+    return (
+        <a href={href} {...props}>
+            {children}
+        </a>
+    )
+}
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
     return {
         Callout,
+        a: MarkdownLink,
         ...components,
     }
 }
